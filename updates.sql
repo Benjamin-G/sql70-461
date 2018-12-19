e002a568b6dee28301bc8714168050437e1a6512
Benjamin Geyer <brgeyer49@gmail.com>
2018-12-19 17:08:05 -0500

    update with output

--- updates.sql
+++ updates.sql
@@ -2,10 +2,15 @@ select * from tblTransaction where EmployeeNumber = 3
 
 
 begin tran
-select * from tblTransaction where EmployeeNumber = 194
+
+--select * from tblTransaction where EmployeeNumber = 194
 update tblTransaction
 set EmployeeNumber = 194
+output inserted.*, deleted.*
 from tblTransaction
 where EmployeeNumber in (3,5,7,9)
-select * from tblTransaction where EmployeeNumber = 194
+--select * from tblTransaction where EmployeeNumber = 194
+
 rollback tran
+
+
